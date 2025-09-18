da9fd0fefb2bbb06e34a94dcaea9bea8ae7a7c18
Clickbaithate <gg.guzzman1514@gmail.com>
2025-09-17 21:28:51 -0500

    ROUTE: Added room page route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import RoomPage from "./pages/RoomPage";
 
 const App = () => {
   return(
-    <div className="w-full h-screen flex items-center justify-center">
-      <HomePage/>
+    <div className="w-full h-screen flex">
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/:roomId" element={<RoomPage/>} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/RoomPage.jsx
@@ -0,0 +1,9 @@
+const RoomPage = () => {
+  return(
+    <div>
+      Room Page
+    </div>
+  );
+}
+
+export default RoomPage;
