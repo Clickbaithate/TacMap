dbb1a3f8dddced144be0470576a571d41dab5133
Clickbaithate <gael.guzman01@utrgv.edu>
2025-09-26 21:44:43 -0500

    REALTIME: 2nd attempt.

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -3,7 +3,7 @@ import { FaUndo, FaRedo, FaTrash } from "react-icons/fa";
 import { useEffect, useRef, useState, useLayoutEffect } from "react";
 import rough from "roughjs";
 
-const RoomPage = () => {
+const RoomPage = ({ user, socket }) => {
 
   const colorInputRef = useRef(null);
 
@@ -13,6 +13,7 @@ const RoomPage = () => {
   const [elements, setElements] = useState([]);
   const [isDrawing, setIsDrawing] = useState(false);
   const [history, setHistory] = useState([]);
+  const [img, setImg] = useState(null);
 
   const roughGenerator = rough.generator();
   const canvasRef = useRef(null);
@@ -145,6 +146,7 @@ const RoomPage = () => {
   }
 
   useLayoutEffect(() => {
+    if (!canvasRef.current) return;
     const roughCanvas = rough.canvas(canvasRef.current);
 
     if (elements.length > 0) {
@@ -164,10 +166,18 @@ const RoomPage = () => {
         )
       }
     })
+
+    const canvasImage = canvasRef.current.toDataURL();
+    socket.emit("whiteboardData", {canvasImage});
+
   }, [elements]);
 
   useEffect(() => {
+    if (!user?.presenter) return;
+
     const canvas = canvasRef.current;
+    if (!canvas) return;
+
     canvas.height = window.innerHeight * 2;
     canvas.width = window.innerWidth * 2;
     const ctx = canvas.getContext("2d");
@@ -178,6 +188,14 @@ const RoomPage = () => {
   }, []);
 
   useEffect(() => {
+    socket.on("whiteboardDataResponse", (data) => {
+      setImg(data.img);
+      console.log("HEY")
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!ctxRef.current) return;
     ctxRef.current.strokeStyle = color;
   }, [color]);
 
@@ -191,58 +209,68 @@ const RoomPage = () => {
       {/* Options */}
       <div className="w-full h-full flex flex-col items-center justify-start">
         {/* Top Bar */}
-        <div className="w-full h-16 flex mb-6">
-          {/* Tools and Color */}
-          <div className="w-full h-full flex items-center justify-start space-x-4 pl-2 md:pl-24">
-            <div className="flex items-center space-x-2">
-              <div className={`border-2 p-1 rounded-md ${tool === "pencil" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("pencil")}><FaPaintbrush/></div>
-              <div className={`border-2 p-1 rounded-md ${tool === "line" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("line")}><FaArrowRight/></div>
-              <div className={`border-2 p-1 rounded-md ${tool === "shape" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("shape")}><FaShapes/></div>
-            </div>
-            {/* Custom Color Picker */}
-            <div className="flex items-center cursor-pointer px-2 py-1 bg-white rounded-md border border-gray-400" onClick={handleColorClick}>
-              <div className="w-6 h-6 rounded-full border border-gray-400" style={{ backgroundColor: color }}/>
-              <FaCaretDown className="ml-2 text-gray-700" />
-            </div>
-            {/* Invisible Color Picker */}
-            <input type="color" ref={colorInputRef} value={color} onChange={handleColorChange} className="invisible absolute top-34" />
-          </div>
-          {/* Edits */}
-          <div className="w-full h-full flex items-center justify-end space-x-4 pr-2 md:pr-24">
-            <div className="flex items-center justify-center space-x-2">
-              {/* Undo Button */}
-              <button 
-                className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" 
-                disabled={elements.length === 0}
-                onClick={() => undo()}
-              >
-                <FaUndo/>
-              </button>
-              {/* Redo Button */}
-              <button 
-                className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" 
-                disabled={history.length < 1}
-                onClick={() => redo()}
-              >
-                <FaRedo/>
-              </button>
-            </div>
-            <div className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer hover:text-red-500" onClick={handleCanvasCLear}><FaTrash/></div>
-          </div>
-        </div>
+        { 
+          user?.presenter ?
+            <div className="w-full h-16 flex mb-6">
+              {/* Tools and Color */}
+              <div className="w-full h-full flex items-center justify-start space-x-4 pl-2 md:pl-24">
+                <div className="flex items-center space-x-2">
+                  <div className={`border-2 p-1 rounded-md ${tool === "pencil" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("pencil")}><FaPaintbrush/></div>
+                  <div className={`border-2 p-1 rounded-md ${tool === "line" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("line")}><FaArrowRight/></div>
+                  <div className={`border-2 p-1 rounded-md ${tool === "shape" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("shape")}><FaShapes/></div>
+                </div>
+                {/* Custom Color Picker */}
+                <div className="flex items-center cursor-pointer px-2 py-1 bg-white rounded-md border border-gray-400" onClick={handleColorClick}>
+                  <div className="w-6 h-6 rounded-full border border-gray-400" style={{ backgroundColor: color }}/>
+                  <FaCaretDown className="ml-2 text-gray-700" />
+                </div>
+                {/* Invisible Color Picker */}
+                <input type="color" ref={colorInputRef} value={color} onChange={handleColorChange} className="invisible absolute top-34" />
+              </div>
+              {/* Edits */}
+              <div className="w-full h-full flex items-center justify-end space-x-4 pr-2 md:pr-24">
+                <div className="flex items-center justify-center space-x-2">
+                  {/* Undo Button */}
+                  <button 
+                    className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" 
+                    disabled={elements.length === 0}
+                    onClick={() => undo()}
+                  >
+                    <FaUndo/>
+                  </button>
+                  {/* Redo Button */}
+                  <button 
+                    className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" 
+                    disabled={history.length < 1}
+                    onClick={() => redo()}
+                  >
+                    <FaRedo/>
+                  </button>
+                </div>
+                <div className="border-2 py-1 px-2 rounded-md border-gray-400 cursor-pointer hover:text-red-500" onClick={handleCanvasCLear}><FaTrash/></div>
+              </div>
+            </div> : 
+          null
+        }
         {/* Canvas */}
-        <div className="w-[90%] h-1/2 md:h-3/4 border-4 rounded-xl bg-gray-100 overflow-hidden">
-          <canvas 
-            className="touch-none" 
-            ref={canvasRef}
-            onPointerDown={handleMouseDown}
-            onPointerMove={handleMouseMove}
-            onPointerUp={handleMouseUp}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-          />
-        </div>
+
+        { user?.presenter ?
+          <div className="w-[90%] h-1/2 md:h-3/4 border-4 rounded-xl bg-gray-100 overflow-hidden">
+            <canvas 
+              className="touch-none" 
+              ref={canvasRef}
+              onPointerDown={handleMouseDown}
+              onPointerMove={handleMouseMove}
+              onPointerUp={handleMouseUp}
+              onMouseDown={handleMouseDown}
+              onMouseMove={handleMouseMove}
+              onMouseUp={handleMouseUp}
+            />
+          </div> :
+          <div className="w-[90%] h-1/2 md:h-3/4 border-4 rounded-xl bg-gray-100 overflow-hidden">
+            <img src={img} alt="Realtime Image" />
+          </div>
+        }
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -5,14 +5,27 @@ const server = require("http").createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let roomIdGlobal, imgGlobal;
+
 io.on("connection", (socket) => {
   socket.on("userJoined", (data) => {
     const { name, userId, roomId, host, presenter } = data;
+    roomIdGlobal = roomId;
     socket.join(roomId);
     socket.emit("userIsJoined", { success: true });
+    socket.broadcast.to(roomId).emit("whiteboardDataResponse", {
+      img: imgGlobal
+    });
 
     const usersInRoom = io.sockets.adapter.rooms.get(roomId);
     console.log(`Users in room ${roomId}:`, usersInRoom ? [...usersInRoom] : []);
+  });
+
+  socket.on("whiteboardData", (data) => {
+    imgGlobal = data.canvasImage;
+    socket.broadcast.to(roomIdGlobal).emit("whiteboardDataResponse", {
+      img: data.canvasImage
+    });
   });
 });
 
