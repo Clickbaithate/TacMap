419615801f391d4970ea8cdaf45570f075d83360
Clickbaithate <gael.guzman01@utrgv.edu>
2025-10-01 21:12:56 -0500

    UI: Fixing toolbar buttons.

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -6,11 +6,11 @@ const Toolbar = ({ tool, setTool, handleColorClick, handleColorChange, color, co
   return(
     <div className="w-full h-16 flex mb-6 justify-between px-6">
       <div className="flex items-center space-x-2">
-        <button className={`border-2 p-1 rounded-md ${tool === "pencil" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("pencil")}><FaPaintbrush /></button>
-        <button className={`border-2 p-1 rounded-md ${tool === "line" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("line")}><FaArrowRight /></button>
-        <button className={`border-2 p-1 rounded-md ${tool === "shape" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("shape")}><FaShapes /></button>
+        <button className={`border-2 p-1 rounded-md cursor-pointer hover:text-yellow-500 ${tool === "pencil" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("pencil")}><FaPaintbrush /></button>
+        <button className={`border-2 p-1 rounded-md cursor-pointer hover:text-purple-500 ${tool === "line" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("line")}><FaArrowRight /></button>
+        <button className={`border-2 p-1 rounded-md cursor-pointer hover:text-green-500 ${tool === "shape" ? "bg-gray-300 border-blue-500" : "border-gray-400"}`} onClick={() => setTool("shape")}><FaShapes /></button>
 
-        <div className="flex items-center cursor-pointer px-2 py-1 bg-white rounded-md border border-gray-400" onClick={handleColorClick}>
+        <div className="flex items-center cursor-pointer px-2 py-1 bg-white rounded-md border border-gray-400 hover:border-red-500" onClick={handleColorClick}>
           <div className="w-6 h-6 rounded-full border border-gray-400" style={{ backgroundColor: color }} />
           <FaCaretDown className="ml-2 text-gray-700" />
           <input type="color" ref={colorInputRef} value={color} onChange={handleColorChange} className="hidden" />
@@ -18,9 +18,9 @@ const Toolbar = ({ tool, setTool, handleColorClick, handleColorChange, color, co
       </div>
 
       <div className="flex items-center space-x-2">
-        <button disabled={elements.length === 0} onClick={undo} className="border-2 py-1 px-2 rounded-md border-gray-400"><FaUndo /></button>
-        <button disabled={history.length < 1} onClick={redo} className="border-2 py-1 px-2 rounded-md border-gray-400"><FaRedo /></button>
-        <button onClick={clearCanvas} className="border-2 py-1 px-2 rounded-md border-gray-400 hover:text-red-500"><FaTrash /></button>
+        <button disabled={elements.length === 0} onClick={undo} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-blue-500"><FaUndo /></button>
+        <button disabled={history.length < 1} onClick={redo} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-blue-500"><FaRedo /></button>
+        <button onClick={clearCanvas} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-red-500"><FaTrash /></button>
       </div>
     </div>
   );
