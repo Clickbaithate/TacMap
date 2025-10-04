5faa0b690443d62826c16c0b14424f38a9d1f2e2
Clickbaithate <gael.guzman01@utrgv.edu>
2025-10-04 09:14:25 -0500

    UI: Minor button fixes.

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,6 +1,7 @@
 import { FaPaintbrush, FaArrowRight, FaShapes, FaCaretDown } from "react-icons/fa6";
 import { FaUndo, FaRedo, FaTrash } from "react-icons/fa";
 
+// TODO: Add disabled cursor icon when hovering over disabled button
 const Toolbar = ({ tool, setTool, handleColorClick, handleColorChange, color, colorInputRef, elements, undo, redo, clearCanvas }) => {
   
   return(
@@ -18,9 +19,9 @@ const Toolbar = ({ tool, setTool, handleColorClick, handleColorChange, color, co
       </div>
 
       <div className="flex items-center space-x-2">
-        <button disabled={elements.length === 0} onClick={undo} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-blue-500"><FaUndo /></button>
-        <button disabled={history.length < 1} onClick={redo} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-blue-500"><FaRedo /></button>
-        <button onClick={clearCanvas} className="border-2 py-1 px-2 rounded-md cursor-pointer border-gray-400 hover:text-red-500"><FaTrash /></button>
+        <button disabled={elements.length === 0} onClick={undo} className="border-2 py-1 px-2 rounded-md cursor-pointer disabled:opacity-50 disabled:hover:text-black border-gray-400 hover:text-blue-500"><FaUndo /></button>
+        <button disabled={history.length < 1} onClick={redo} className="border-2 py-1 px-2 rounded-md cursor-pointer disabled:opacity-50 disabled:hover:text-black border-gray-400 hover:text-blue-500"><FaRedo /></button>
+        <button disabled={elements.length === 0} onClick={clearCanvas} className="border-2 py-1 px-2 rounded-md cursor-pointer disabled:opacity-50 disabled:hover:text-black border-gray-400 hover:text-red-500"><FaTrash /></button>
       </div>
     </div>
   );
